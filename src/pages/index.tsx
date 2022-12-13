9197ee3ec0d62d4c17b05e96b9c531ed2f02a74b
hujunlei <hujunlei@iauto.com>
2022-12-13 17:32:17 +0800

    dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
-import styles from './index.less';
+import { useCallback, useState, PureComponent, useMemo } from "react";
+import { Modal, Button } from "antd";
+import styles from "./index.less";
 
 export default function IndexPage() {
+  const [data, setData] = useState(0);
+  const childProps = useMemo(
+    {
+      callback: () => {
+        console.log(data);
+      },
+    },
+    []
+  );
   return (
     <div>
-      <h1 className={styles.title}>我是根组件</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        changeModal
+      </button>
+      <Child {...childProps} />
     </div>
   );
+}
+
+class Child extends PureComponent {
+  render() {
+    return <div onClick={this.props.callback}>child</div>;
+  }
 }
